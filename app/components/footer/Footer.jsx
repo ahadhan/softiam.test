21ab17e0f03b251b0dd687019ac98ca4b343d66f
Abdul Ahad Khan <ahadkhanmay@gmail.com>
2024-12-17 01:10:16 +0500

    final

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,91 +1,11 @@
-// import React from 'react';
-// import Link from 'next/link';
-// import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
-
-// const Footer = () => {
-//     return (
-//         <div className='bg-[#12141E] py-10'>
-//             <div className="main-container mx-auto  ">
-//                 <div className='flex'>
-//                 <div className='basis-1/2'>
-//                     {/* First section */}
-//                     <div className=" text-white font-roboto  mb-3 text-3xl">
-//                         <h1 className='text-5xl font-playfair font-bold mb-3'>softiam. </h1>
-//                         <h1 className='text-blue-500'>IT Solutions & Technology</h1>
-//                     </div>
-
-//                     <div className="icons flex text-white gap-5 text-2xl my-8">
-//                         <span >
-//                             <FaFacebook />
-//                         </span>
-//                         <span >
-//                             <FaInstagram></FaInstagram>
-//                         </span>
-//                         <span><FaTwitter></FaTwitter></span>
-//                         <span><FaLinkedin></FaLinkedin></span>
-
-//                     </div>
-
-//                     <div className="button mt-10">
-//                         <button className="material-bubble bg-secondaryColor text-white px-4 py-2 rounded-lg border border-blue-700 font-semibold">
-//                             Get Quote
-//                         </button>
-//                     </div>
-
-//                 </div>
-
-//                 {/* Second section */}
-
-//                 <div className='basis-1/2 mt-3'>
-//                     <div className="para text-white text-xl mb-12">At SOFTIAM, we provide cutting-edge digital solutions tailored to meet your business needs, ensuring you stay ahead in the digital world.</div>
-
-
-
-
-//                     <div className="copyright text-white text-xl flex gap-3">
-//                         <p>&#169; 2024 - All rights reserved by </p>
-//                         <p className='text-blue-500 font-bold'> Softiam</p>
-
-//                     </div>
-
-
-//                 </div>
-//                 </div>
-
-//                 <div className=" flex space-x-12  text-white justify-center">
-//                         <Link href="/" className=" hover:text-blue-300 hover:underline duration-300 font-[600]">
-//                             Home
-//                         </Link>
-//                         <Link href="/about" className=" hover:text-blue-300 hover:underline duration-300 font-[600]">
-//                             About
-//                         </Link>
-//                         <Link href="/services" className=" hover:text-blue-300 hover:underline duration-300 font-[600]">
-//                             Services
-//                         </Link>
-//                         <Link href="/contact" className=" hover:text-blue-300 hover:underline duration-300 font-[600]">
-//                             FAQ
-//                         </Link>
-//                         <Link href="/contact" className=" hover:text-blue-300 hover:underline duration-300 font-[600]">
-//                             Projects
-//                         </Link>
-//                         <Link href="/contact" className=" hover:text-blue-300 hover:underline duration-300 font-[600]">
-//                             Contact Us
-//                         </Link>
-//                     </div>
-//             </div>
-
-//         </div>
-//     )
-// }
-
-// export default Footer
-
 
 
 
 import React from 'react';
 import Link from 'next/link';
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import Logo from "../../assets/images/logo-white.png"
+import Image from 'next/image';
 
 const Footer = () => {
     return (
@@ -94,8 +14,9 @@ const Footer = () => {
                 <div className='flex flex-col lg:flex-row'>
                     <div className='lg:basis-1/2'>
                         {/* First section */}
-                        <div className="text-white font-roboto mb-3 text-3xl">
-                            <h1 className='text-4xl lg:text-5xl font-playfair font-bold mb-3'>softiam. </h1>
+                        <div className="text-white font-roboto mb-3 text-3xl ">
+                            <Image src={Logo} width={300} height={300} className='-ml-5'>
+                            </Image>                            
                             <h1 className='text-blue-500 text-lg lg:text-xl'>IT Solutions & Technology</h1>
                         </div>
 
--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,118 +1,10 @@
-// "use client";
-// import { useEffect, useState } from "react";
-// import Link from "next/link";
-// import logo from "../../assets/images/logo.png";
-// import Image from "next/image";
-
-// export default function Header() {
-//     const [isOpen, setIsOpen] = useState(false);
-//     const [isClient, setIsClient] = useState(false);
-
-//     useEffect(() => {
-//         setIsClient(true); // Runs after hydration
-//     }, []);
-
-//     return isClient ? (
-//         <nav className="bg-white shadow">
-//             <div className=" mx-auto  main-container">
-//                 <div className="flex justify-between items-center h-16">
-//                     <div className="flex items-center w-1/3">
-//                         <Image src={logo} alt="logo" width={150} height={150} />
-//                     </div>
-
-//                     {/* Menu Links */}
-//                     <div className="hidden md:flex space-x-12 font-montserrat ">
-//                         <Link href="/" className="text-black hover:text-blue-300 duration-300 font-[600]">
-//                             Home
-//                         </Link>
-//                         <Link href="/about" className="text-black hover:text-blue-300 duration-300 font-[600]">
-//                             About
-//                         </Link>
-//                         <Link href="/services" className="text-black hover:text-blue-300 duration-300 font-[600]">
-//                             Services
-//                         </Link>
-//                         <Link href="/contact" className="text-black hover:text-blue-300 duration-300 font-[600]">
-//                             FAQ
-//                         </Link>
-//                         <Link href="/contact" className="text-black hover:text-blue-300 duration-300 font-[600]">
-//                             Projects
-//                         </Link>
-//                         <Link href="/contact" className="text-black hover:text-blue-300 duration-300 font-[600]">
-//                             Contact Us
-//                         </Link>
-//                     </div>
-
-//                     {/* Button */}
-//                     <div className="hidden md:block">
-//                         <Link href="/get-started">
-//                             <button className="material-bubble bg-secondaryColor text-white px-4 py-2 rounded-lg border border-blue-700 font-semibold">
-//                                 Know Us
-//                             </button>
-//                         </Link>
-//                     </div>
-
-//                     {/* Mobile Menu Button */}
-//                     <div className="flex sm:hidden">
-//                         <button
-//                             onClick={() => setIsOpen(!isOpen)}
-//                             type="button"
-//                             className="text-white hover:text-blue-300 focus:outline-none"
-//                         >
-//                             <svg
-//                                 className="h-6 w-6"
-//                                 xmlns="http://www.w3.org/2000/svg"
-//                                 fill="none"
-//                                 viewBox="0 0 24 24"
-//                                 stroke="currentColor"
-//                                 aria-hidden="true"
-//                             >
-//                                 <path
-//                                     strokeLinecap="round"
-//                                     strokeLinejoin="round"
-//                                     strokeWidth="2"
-//                                     d={
-//                                         isOpen
-//                                             ? "M6 18L18 6M6 6l12 12"
-//                                             : "M4 6h16M4 12h16M4 18h16"
-//                                     }
-//                                 />
-//                             </svg>
-//                         </button>
-//                     </div>
-//                 </div>
-//             </div>
-
-//             {/* Mobile Menu */}
-//             {isOpen && (
-//                 <div className="md:hidden px-2 pt-2 pb-3 space-y-1 sm:px-3">
-//                     <Link href="/about" className="block text-white hover:text-blue-300 font-bold">
-//                         About
-//                     </Link>
-//                     <Link href="/services" className="block text-white hover:text-blue-300">
-//                         Services
-//                     </Link>
-//                     <Link href="/contact" className="block text-white hover:text-blue-300">
-//                         Contact
-//                     </Link>
-//                     <Link
-//                         href="/get-started"
-//                         className="block text-blue-600 bg-white px-4 py-2 rounded-md hover:bg-gray-200"
-//                     >
-//                         Get Started
-//                     </Link>
-//                 </div>
-//             )}
-//         </nav>
-//     ) : null;
-// }
-
 
 
 
 "use client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo-dark.png";
 import Image from "next/image";
 
 export default function Header() {
@@ -129,7 +21,7 @@ export default function Header() {
                 <div className="flex justify-between items-center h-16 px-4">
                     {/* Logo */}
                     <div className="flex items-center">
-                        <Image src={logo} alt="logo" width={150} height={150} />
+                        <Image src={logo} alt="logo" width={250} height={250} />
                     </div>
 
                     {/* Desktop Menu */}
